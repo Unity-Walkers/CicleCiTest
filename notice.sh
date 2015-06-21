98ca79e2d8bcb9604f9be7251f69c4f15eac713f
tomomille <goto428@gmail.com>
2015-06-21 20:29:59 +0900

    Trial.

--- notice.sh
+++ notice.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST --data-urlencode 'payload={"channel": "#test", "username": "@aerith(CircleCI)", "text": "CircleCI からのメッセージだよ!!"}' https://hooks.slack.com/services/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
+curl -X POST --data-urlencode 'payload={"channel": "#test", "username": "@aerith(CircleCI)", "text": "CircleCI からのメッセージだよ!! https://circleci.com/gh/Unity-Walkers/CicleCiTest"}' https://hooks.slack.com/services/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
