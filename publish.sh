50fc3981533a81e09684ece581bc856fb3bfd8f5
tomomille <goto428@gmail.com>
2015-06-27 11:50:52 +0900

    Trial.

--- publish.sh
+++ publish.sh
@@ -7,7 +7,6 @@ aws --version
 aws configure set aws_access_key_id $ACCESSKEY
 aws configure set aws_secret_access_key $SECRETKEY
 aws configure set default.region tokyo
-aws configure set default.output json
+#aws configure set default.output json
 
-echo "SAVING NEW DOCKERRUNFILE: $DOCKERRUN_FILE"
 aws s3 cp --debug ./circle.yml s3://unitywalkers/circle.yml
