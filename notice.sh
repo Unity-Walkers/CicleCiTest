a3bf278e23fe1bb745f1b99c8d05299c846a8419
tomomille <goto428@gmail.com>
2015-06-27 14:26:01 +0900

    Trial.

--- notice.sh
+++ notice.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST --data-urlencode 'payload={"channel": "#test", "username": "@aerith(CircleCI)", "text": "CircleCI からのメッセージだよ!! https://circleci.com/gh/Unity-Walkers/CicleCiTest"}' https://hooks.slack.com/services/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
+curl -X POST --data-urlencode 'payload={"channel": "#test", "username": "aerith(CircleCI)", "text": "アプリが出来ました!! http://unitywalkers.s3-website-ap-northeast-1.amazonaws.com/App/app_${CIRCLE_BUILD_NUM}.zip"}' https://hooks.slack.com/services/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
--- publish.sh
+++ publish.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#ACCESSKEY="AKIAIYBROA73ET5Y4R2A"
-#SECRETKEY="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"
-
 aws --version
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
@@ -10,4 +7,3 @@ aws configure set default.region ${AWS_DEFAULT_REGION}
 aws configure set default.output json
 
 aws s3 cp --debug ./app.zip s3://unitywalkers/App/app_${CIRCLE_BUILD_NUM}.zip
-#aws s3 ls s3://unitywalkers
