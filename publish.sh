467b5218b8d07671db20ede358c158823e4cef53
tomomille <goto428@gmail.com>
2015-06-27 23:03:52 +0900

    Trial.

--- publish.sh
+++ publish.sh
@@ -6,4 +6,5 @@ aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws configure set default.region ${AWS_DEFAULT_REGION}
 aws configure set default.output json
 
+# unitywalkers2 ‚Í oregon
 aws s3 cp --debug ./app.zip s3://unitywalkers2/App/app_${CIRCLE_BUILD_NUM}.zip
