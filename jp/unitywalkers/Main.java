beb0bc6ff25016cb74ae4f8dcadb3d4bee26309a
tomomille <goto428@gmail.com>
2015-06-27 21:59:32 +0900

    Add tomomille's message.

--- jp/unitywalkers/Main.java
+++ jp/unitywalkers/Main.java
@@ -6,7 +6,6 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("Hello,World!!");
-		System.out.println("You copy? I copy!");
-		System.out.println("VALKYRIE PROFILE");
+		System.out.println("tomomille");
 	}
 }
