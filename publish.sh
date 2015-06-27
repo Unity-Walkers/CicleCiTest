e2cd750d24cc6f15ba838e39aa49ccb50f250850
tomomille <goto428@gmail.com>
2015-06-27 12:36:43 +0900

    Trial.

--- publish.sh
+++ publish.sh
@@ -6,8 +6,8 @@ SECRETKEY="+Kw5QvofW/kGQNEe6LhfikK5t7tPjZ6pbWWqFvud"
 aws --version
 aws configure set aws_access_key_id $ACCESSKEY
 aws configure set aws_secret_access_key $SECRETKEY
-aws configure set default.region tokyo
-#aws configure set default.output json
+aws configure set default.region Tokyo
+aws configure set default.output json
 
 aws s3 cp --debug ./circle.yml s3://unitywalkers/circle.yml
 #aws s3 ls s3://unitywalkers
