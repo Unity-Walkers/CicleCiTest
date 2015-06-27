fe201b398ea05818f882f967f664fef3b1d6dc5f
tomomille <goto428@gmail.com>
2015-06-27 14:37:03 +0900

    Trial.

--- notice.sh
+++ notice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CURL_PAYLOAD="payload={\"channel\": \"#test\", \"username\": \"aerith(CircleCI)\", \"text\": \"アプリが出来ました!! http://unitywalkers.s3-website-ap-northeast-1.amazonaws.com/App/app_${CIRCLE_BUILD_NUM}.zip\"}"
+CURL_PAYLOAD="\'payload={\"channel\": \"#test\", \"username\": \"aerith(CircleCI)\", \"text\": \"アプリが出来ました!! http://unitywalkers.s3-website-ap-northeast-1.amazonaws.com/App/app_${CIRCLE_BUILD_NUM}.zip\"}\'"
 
 curl -X POST --data-urlencode ${CURL_PAYLOAD} https://hooks.slack.com/services/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
